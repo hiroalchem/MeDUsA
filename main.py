b3949ab38d8ea83e9e07d44059842f3a8cc5f6c9
hiroalchem <h.kawai888@gmail.com>
2022-11-04 13:23:37 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,214 @@
+import os
+import random
+import shutil
+from pathlib import Path
+
+import pandas as pd
+import wx
+import rstr
+
+from tensorflow.python.keras.models import load_model
+
+from utils import save_preprocessed_images, predict, load_folder, bce_dice_loss, dice_coeff
+
+
+class Main(wx.Frame):
+    def __init__(self, parent, sid, title):
+        wx.Frame.__init__(self, parent, sid, title, size=(600, 360))
+        panel = wx.Panel(self, id=sid)
+
+        self.button_labels = ["choose dataset folder", "choose mask output folder", "choose result output folder"]
+
+        # dataset folder
+        wx.StaticText(panel, wx.ID_ANY, label="data folder", pos=(10, 10))
+        self.raw = wx.TextCtrl(panel, wx.ID_ANY, pos=(10, 30), size=(320, 20))
+        choose_button_raw = wx.Button(panel, label=self.button_labels[0], pos=(350, 25))
+
+        # mask output folder
+        wx.StaticText(panel, wx.ID_ANY, label="mask output folder", pos=(10, 60))
+        self.mask = wx.TextCtrl(panel, wx.ID_ANY, pos=(10, 80), size=(320, 20))
+        choose_button_mask_output = wx.Button(panel, label=self.button_labels[1], pos=(350, 75))
+
+        # output folder
+        wx.StaticText(panel, wx.ID_ANY, label="result output folder", pos=(10, 110))
+        self.out = wx.TextCtrl(panel, wx.ID_ANY, pos=(10, 130), size=(320, 20))
+        choose_button_output = wx.Button(panel, label=self.button_labels[2], pos=(350, 125))
+
+        self.texts = [self.raw, self.mask, self.out]
+
+        self.Bind(wx.EVT_BUTTON, self.choose_folder)
+
+        wx.StaticText(panel, wx.ID_ANY, label="model hdf5", pos=(10, 160))
+        self.modelfile = wx.TextCtrl(panel, wx.ID_ANY, pos=(10, 180), size=(320, 20))
+        choose_button_modelfile = wx.Button(panel, label="choose file", pos=(350, 175))
+        choose_button_modelfile.Bind(wx.EVT_BUTTON, self.choose_model_file)
+
+        wx.StaticText(panel, wx.ID_ANY, label="percent of images to output detection points", pos=(10, 215))
+        self.ratio = wx.SpinCtrl(panel, wx.ID_ANY, min=0, max=100, pos=(10, 240), size=(120, 35))
+
+        button = wx.Button(panel, label='start', pos=(100, 280))
+        button.Bind(wx.EVT_BUTTON, self.analyze)
+
+        self.Show(True)
+        self.Centre()
+
+    def choose_folder(self, event):
+        """ folder selection event"""
+
+        folder = wx.DirDialog(self, style=wx.DD_DEFAULT_STYLE | wx.DD_DIR_MUST_EXIST,
+                              message="select folder")
+
+        if folder.ShowModal() == wx.ID_OK:
+            folder_path = folder.GetPath()
+        folder.Destroy()
+
+        self.texts[self.button_labels.index(event.GetEventObject().GetLabel())].SetValue(folder_path)
+
+    def choose_model_file(self, event):
+        """ model file (hdf5) selection event """
+
+        file = wx.FileDialog(self, "Open hdf5 file", wildcard="hdf files (*.hdf5)|*.hdf5",
+                             style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+
+        if file.ShowModal() == wx.ID_OK:
+            file_path = file.GetPath()
+        file.Destroy()
+        self.modelfile.SetValue(file_path)
+
+    def analyze(self, event):
+
+        temp_dir = rstr.xeger(r'^[0-9]{2}[0-9a-zA-Z0-9]{10}')
+
+        data_dir = self.raw.GetValue()
+        out_dir = self.mask.GetValue()
+
+        print(data_dir)
+
+        data_dirs = load_folder(data_dir)
+
+        num_dirs = len(data_dirs)
+        print(num_dirs)
+
+        if num_dirs == 0:
+            wx.Exit()
+        else:
+            pass
+
+        # progress dialog
+        dlg = wx.ProgressDialog('Processing images', f'0/{num_dirs} folders completed', num_dirs,
+                                style=wx.PD_APP_MODAL | wx.PD_SMOOTH | wx.PD_AUTO_HIDE | wx.PD_CAN_ABORT)
+
+        # preprocess
+
+        print('start processing')
+
+        for a, d in enumerate(data_dirs):
+            save_preprocessed_images(d, temp_dir)
+
+            # update
+            alive, skip = dlg.Update(a+1, f'{a+1}/{num_dirs} folders completed')
+            if not alive:
+                if os.path.exists(temp_dir):
+                    shutil.rmtree(temp_dir)
+                else:
+                    pass
+                wx.MessageBox(u'aborted.')
+                break
+        dlg.Destroy()
+
+        if alive:
+            pass
+
+        # start prediction
+
+        model = load_model(self.modelfile.GetValue(), custom_objects={'bce_dice_loss': bce_dice_loss, 'dice_coeff': dice_coeff})
+        data_dirs = load_folder(os.path.join(temp_dir, 'images'))
+        num_dirs = len(data_dirs)
+        # progress
+        dlg = wx.ProgressDialog('Predicting', f'0/{num_dirs} folders completed', num_dirs,
+                                style=wx.PD_APP_MODAL | wx.PD_SMOOTH | wx.PD_AUTO_HIDE | wx.PD_CAN_ABORT)
+
+        for a, d in enumerate(data_dirs):
+            predict(d, model, out_dir)
+            # update
+            alive, skip = dlg.Update(a+1, f'{a+1}/{num_dirs} folders completed')
+            if not alive:
+                if os.path.exists(temp_dir):
+                    shutil.rmtree(temp_dir)
+                else:
+                    pass
+                wx.MessageBox(u'aborted.')
+                break
+        dlg.Destroy()
+        if alive:
+            if os.path.exists(temp_dir):
+                shutil.rmtree(temp_dir)
+            else:
+                pass
+
+        # detection
+
+        raw_dir = data_dir
+        mask_dir = out_dir
+        out_dir = self.out.GetValue()
+        data_dirs = load_folder(raw_dir)
+        ratio = self.ratio.GetValue()*0.01
+        h = 0.6
+        c = -4
+        v = 20
+        ft = 'png'
+
+        num_dirs = len(data_dirs)
+
+        if num_dirs == 0:
+            wx.Exit()
+        else:
+            pass
+
+        # sampling dir for output detection result images
+        idx_out = sorted(random.sample(range(num_dirs), k=int(num_dirs * ratio)))
+
+        # progress dialog
+        dlg = wx.ProgressDialog('detecting', f'0/{num_dirs} folders completed', num_dirs,
+                                style=wx.PD_APP_MODAL | wx.PD_SMOOTH | wx.PD_AUTO_HIDE | wx.PD_CAN_ABORT)
+
+        # start detection
+
+        df = pd.DataFrame(columns=['sample_name', 'pred_num'])
+        for a, d in enumerate(data_dirs):
+            imps = sorted(list(d.glob('./**/*tif')))
+            try:
+                images, masks = self.load_images_and_masks(imps, Path(mask_dir), ft)
+            except:
+                print('no mask')
+                continue
+
+            if a in idx_out:
+                count = self.draw_blob4(imps, images, masks, out_dir, h=h, gauss=None, b=31, c=c, d=4, v=v)
+            else:
+                count = self.count_blob4(images, masks, h=h, gauss=None, b=31, c=c, d=4, v=v)
+
+            tmp_se = pd.Series([d.name, count], index=df.columns)
+            df = df.append(tmp_se, ignore_index=True)
+
+            # update
+            alive, skip = dlg.Update(a + 1, f'{a + 1}/{num_dirs} folders completed')
+            if not alive:
+                wx.MessageBox(u'aborted.')
+                df.to_csv(os.path.join(out_dir, 'result.csv'))
+                break
+        dlg.Destroy()
+        if alive:
+            os.makedirs(out_dir, exist_ok=True)
+            df.to_csv(os.path.join(out_dir, 'result.csv'))
+            wx.MessageBox(u'done.')
+
+
+def main():
+    app = wx.App(False)
+    Main(None, wx.ID_ANY, "MeDUsA")
+    app.MainLoop()
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils.py
@@ -0,0 +1,178 @@
+import os
+from pathlib import Path
+
+import cv2
+import numpy as np
+import scipy.ndimage as ndi
+from skimage import feature, morphology, img_as_float
+from skimage.morphology import reconstruction
+from skimage.draw import rectangle
+from tensorflow.python.keras import backend as K
+from tensorflow.python.keras.losses import binary_crossentropy
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tqdm import tqdm
+
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss(y_true, y_pred):
+    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+
+
+def load_folder(data_d):
+    p = Path(data_d)
+    data_dirs = sorted(list(p.glob('./*_raw')))
+    return data_dirs
+
+
+def save_preprocessed_images(data_dir, temp_dir):
+    images, filenames = load_x_gray_3slices(str(data_dir))
+    os.makedirs(os.path.join(temp_dir, 'images', data_dir.name, 'images'))
+    for idx, filename in enumerate(filenames):
+        cv2.imwrite(os.path.join(temp_dir, 'images', data_dir.name, 'images',
+                                 os.path.splitext((os.path.basename(filename)))[0] + '.png'), images[idx])
+
+
+def load_x_gray_3slices(folder_path):
+    print('load files')
+    x_files = []
+    for file in os.listdir(folder_path):
+        base, ext = os.path.splitext(file)
+        if ext == '.tif':
+            x_files.append(file)
+        else:
+            pass
+
+    x_files.sort()
+
+    print(f'{len(x_files)} files')
+
+    images = []
+    for i, image_file in tqdm(enumerate(x_files)):
+        if i == 0:
+            image1 = cv2.equalizeHist(cv2.imread(folder_path + os.sep + x_files[i], cv2.IMREAD_GRAYSCALE))
+            image0 = np.zeros_like(image1)
+            image2 = cv2.equalizeHist(cv2.imread(folder_path + os.sep + x_files[i + 1], cv2.IMREAD_GRAYSCALE))
+        elif i == (len(x_files) - 1):
+            image0 = cv2.equalizeHist(cv2.imread(folder_path + os.sep + x_files[i - 1], cv2.IMREAD_GRAYSCALE))
+            image1 = cv2.equalizeHist(cv2.imread(folder_path + os.sep + x_files[i], cv2.IMREAD_GRAYSCALE))
+            image2 = np.zeros_like(image1)
+        else:
+            image0 = cv2.equalizeHist(cv2.imread(folder_path + os.sep + x_files[i - 1], cv2.IMREAD_GRAYSCALE))
+            image1 = cv2.equalizeHist(cv2.imread(folder_path + os.sep + x_files[i], cv2.IMREAD_GRAYSCALE))
+            image2 = cv2.equalizeHist(cv2.imread(folder_path + os.sep + x_files[i + 1], cv2.IMREAD_GRAYSCALE))
+        image3ch = np.stack((image0, image1, image2), axis=2)
+        images.append(image3ch)
+        # image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE))
+    return images, x_files
+
+
+def predict(data_dir, model, out_dir):
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_generator = test_datagen.flow_from_directory(str(data_dir), target_size=(512, 512), class_mode=None,
+                                                      shuffle=False, batch_size=1)
+    filenames = test_generator.filenames
+    nb_samples = len(filenames)
+    y_pred = model.predict_generator(test_generator, nb_samples, verbose=1)
+    os.makedirs(os.path.join(out_dir, data_dir.name), exist_ok=True)
+    for i, y in enumerate(y_pred):
+        # y = denormalize_y(y[:,:,-1]).astype('uint8')
+        y = (y[:, :, 1] * 255.).astype('uint8')
+        _, y = cv2.threshold(y, 127, 255, cv2.THRESH_BINARY)
+        cv2.imwrite(os.path.join(out_dir, data_dir.name, os.path.basename(filenames[i])), y)
+
+
+def load_images_and_masks(imps, pm, ft):
+    images = np.zeros((len(imps), 512, 512), np.uint8)
+    masks = np.zeros((len(imps), 512, 512), np.uint8)
+    for i in range(len(imps)):
+        imr = cv2.imread(str(imps[i]), 0)
+        if ft == "png":
+            imm = cv2.imread(str((pm / imps[i].parent.name / imps[i].name).with_suffix(".png")), 0)
+        else:
+            imm = cv2.imread(str(pm / imps[i].parent.name / imps[i].name), 0)
+        images[i] = imr
+        masks[i] = imm
+    return images, masks
+
+
+def make_masked_images_from_array(images, masks):
+    images_masked = np.zeros_like(images)
+    for i in range(len(images)):
+        images_masked[i] = cv2.bitwise_and(images[i], images[i], mask=masks[i])
+    return images_masked
+
+
+def blob_detection(images, masks, h, gauss, b, c, d):
+    images3 = np.zeros_like(images)
+    for i, image in enumerate(images):
+        if gauss:
+            image = ndi.gaussian_filter(image, sigma=gauss)
+        else:
+            pass
+        image = img_as_float(image)
+        h = h
+        seed = image - h
+        mask = image
+        dilated = reconstruction(seed, mask, method='dilation')
+        image = image - dilated
+        image = (image * 255).astype('uint8')
+        images3[i] = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, b, c)
+    images4 = ndi.gaussian_filter(images3, sigma=1)
+    images4 = (images4 >= 127).astype('uint8')
+    images4 = make_masked_images_from_array(images4, masks)
+    distance = ndi.distance_transform_edt(images4)
+    local_max = feature.peak_local_max(distance, indices=False, labels=images4, min_distance=d,
+                                       exclude_border=False)
+    markers = ndi.label(local_max, structure=np.ones((3, 3, 3)))[0]
+    labels = morphology.watershed(-distance, markers, mask=images4)
+    return labels
+
+
+def count_blob4(images, masks, h=0.6, gauss=None, b=31, c=-4, d=4, v=24):
+    labels = blob_detection(images, masks, h, gauss, b, c, d)
+    if labels.max() > 1500:
+        return 0
+    else:
+        _, nums = np.unique(labels, return_counts=True)
+        return np.count_nonzero(nums > v) - 1
+
+
+def draw_blob4(imps, images, masks, out_dir, h=0.6, gauss=None, b=31, c=-4, d=4, v=24):
+    labels = blob_detection(images, masks, h, gauss, b, c, d)
+    if labels.max() > 1500:
+        return 0
+    else:
+        uni, nums = np.unique(labels, return_counts=True)
+        centers = []
+        for i in range(len(uni) - 1):
+            if nums[i + 1] > v:
+                centers.append(ndi.measurements.center_of_mass(labels == uni[i + 1]))
+            else:
+                pass
+        dr = np.zeros_like(images)
+        for c in centers:
+            z, raw, col = c
+            start = (int(z) - 2, int(raw) - 3, int(col) - 3)
+            end = (int(z) + 2, int(raw) + 3, int(col) + 3)
+            z, r, c = rectangle(start, end, shape=dr.shape)
+            dr[z, r, c] = 255
+
+        for i in range(len(dr)):
+            os.makedirs(os.path.join(out_dir, imps[i].parent.name), exist_ok=True)
+            cv2.imwrite(os.path.join(out_dir, imps[i].parent.name, imps[i].name), dr[i])
+
+        return len(centers)
